6ce919ff9c3a5038932f0e41213bc46e7c494aee
rabobahago <rabobahago@gmail.com>
2021-08-09 14:40:31 +0100

    maximum numbers of character

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,44 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
-
+// first solution
+// function maxChar(str) {
+//   let charMap = {};
+//   let max = 0;
+//   let charMax = "";
+//   for (let char of str) {
+//     if (charMap[char]) {
+//       charMap[char]++;
+//     } else {
+//       charMap[char] = 1;
+//     }
+//   }
+//   for (let char in charMap) {
+//     if (charMap[char] > max) {
+//       max = charMap[char];
+//       charMax = char;
+//     }
+//   }
+//   return charMax;
+// }
+//Second Solution
+function maxChar(str) {
+  let charMap = {};
+  let max = 0;
+  let character = "";
+  str.split("");
+  for (let i = 0; i < str.length; i++) {
+    if (!charMap[str[i]]) {
+      charMap[str[i]] = 1;
+    }
+    charMap[str[i]] = charMap[str[i]] + 1;
+  }
+  for (let elem of Object.keys(charMap)) {
+    if (charMap[elem] > max) {
+      max = charMap[elem];
+      character = elem;
+    }
+  }
+  return character;
+}
 module.exports = maxChar;
--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,9 +8,17 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
+//first solution
+// function reverseInt(n) {
+//   const reversed = n.toString().split("").reverse().join("");
+//   return parseInt(reversed) * Math.sign(n);
+// }
+
+//second solution
 function reverseInt(n) {
   const reversed = n.toString().split("").reverse().join("");
-  return parseInt(reversed) * Math.sign(n);
+  if (n < 0) return parseInt(reversed) * -1;
+  return parseInt(reversed);
 }
 
 module.exports = reverseInt;
