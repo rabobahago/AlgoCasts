ad7e9173bc0046835b021b78619f295d415eaa6f
rabobahago <rabobahago@gmail.com>
2021-08-09 17:17:54 +0100

    anagrams challenge completed

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -7,7 +7,35 @@
 //   anagrams('rail safety', 'fairy tales') --> True
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
+//first solution
+function anagrams(stringA, stringB) {
+  const aString = cleanAnagram(stringA);
+  const bString = cleanAnagram(stringB);
+  return aString === bString;
+}
+function cleanAnagram(str) {
+  return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
+}
 
-function anagrams(stringA, stringB) {}
-
+// second solution
+// function anagrams(stringA, stringB) {
+//   let aCharMap = buidCharMaP(stringA);
+//   let bCharMap = buidCharMaP(stringB);
+//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//     return false;
+//   }
+//   for (let char in aCharMap) {
+//     if (aCharMap[char] !== bCharMap[char]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+// function buidCharMaP(str) {
+//   let charMap = {};
+//   for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+//   return charMap;
+// }
 module.exports = anagrams;
