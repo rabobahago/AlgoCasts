530d35989235da63a44891e1d258f4e2a380f9ac
rabobahago <rabobahago@gmail.com>
2021-08-09 11:03:18 +0100

    reversed string difference solutions added

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,13 +6,35 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// first solution
 function reverse(str) {
-  let newArr = [];
-  const array = str.split("");
-  for (let i = array.length - 1; i >= 0; i--) {
-    newArr.push(array[i]);
-  }
-  return newArr.join("");
+  return str
+    .split("")
+    .reduce((reversed, character) => character + reversed, "");
 }
+
+// second solution
+// function reverse(str) {
+//   let reversed = "";
+//   for (const element of str) {
+//     reversed = element + reversed;
+//   }
+//   return reversed;
+// }
+
+// third solution
+// function reverse(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// Four solution
+// function reverse(str) {
+//   let newArr = [];
+//   const array = str.split("");
+//   for (let i = array.length - 1; i >= 0; i--) {
+//     newArr.push(array[i]);
+//   }
+//   return newArr.join("");
+// }
 
 module.exports = reverse;
