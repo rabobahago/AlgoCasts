dd0ff3d0f25671f492e91ec5c81134553dd66f9f
rabobahago <rabobahago@gmail.com>
2021-08-11 11:48:48 +0100

    capitalise challenge completed

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,26 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let result = str[0].toUpperCase();
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === " ") {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+}
+
+//first solution
+// function capitalize(str) {
+//   str = str.split(" ");
+//   const cap = [];
+//   for (let word of str) {
+//     cap.push(word[0].toUpperCase() + word.slice(1));
+//   }
+//   return cap.join(" ");
+// }
 
 module.exports = capitalize;
