b28ca52e82b5d37e15e99afab12b44fc1d366ceb
rabobahago <rabobahago@gmail.com>
2021-08-13 06:02:17 +0100

    pyramid challenge completed

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -13,7 +13,37 @@
 //       '  #  '
 //       ' ### '
 //       '#####'
+//first solution
+// function pyramid(n) {
+//   const midPoint = Math.floor((2 * n - 1) / 2);
+//   for (let row = 0; row < n; row++) {
+//     let level = "";
+//     for (let col = 0; col < 2 * n - 1; col++) {
+//       if (midPoint - row <= col && midPoint + row >= col) {
+//         level += "#";
+//       } else {
+//         level += " ";
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
+//second solution
+function pyramid(n, row = 0, level = "") {
+  if (row === n) {
+    return;
+  }
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+  let midPoint = Math.floor((2 * n - 1) / 2);
 
-function pyramid(n) {}
-
+  if (midPoint - row <= level.length && midPoint + row >= level.length) {
+    level += "#";
+  } else {
+    level += " ";
+  }
+  pyramid(n, row, level);
+}
 module.exports = pyramid;
