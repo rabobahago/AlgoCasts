76368a006a6b655ebb8a58ee10ab6bf9b0a898d5
rabobahago <rabobahago@gmail.com>
2021-08-14 15:04:04 +0100

    qfroms challenge completed

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -12,8 +12,36 @@
 //     q.remove(); // returns 1
 //     q.remove(); // returns 2
 
-const Stack = require('./stack');
+const Stack = require("./stack");
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.first = new Stack();
+    this.last = new Stack();
+  }
+  add(record) {
+    this.first.push(record);
+  }
+  remove() {
+    while (this.first.peek()) {
+      this.last.push(this.first.pop());
+    }
+    const record = this.last.pop();
+    while (this.last.peek()) {
+      this.first.push(this.last.pop());
+    }
+    return record;
+  }
+  peek() {
+    while (this.first.peek()) {
+      this.last.push(this.first.pop());
+    }
+    const record = this.last.peek();
+    while (this.last.peek()) {
+      this.first.push(this.last.pop());
+    }
+    return record;
+  }
+}
 
 module.exports = Queue;
