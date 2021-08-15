454fb8c18d206a346cbb5a94faf74f22b5c5a226
rabobahago <rabobahago@gmail.com>
2021-08-15 19:49:15 +0100

    linkedlist challenge completed

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -1,9 +1,104 @@
 // --- Directions
 // Implement classes Node and Linked Lists
 // See 'directions' document
-
-class Node {}
-
-class LinkedList {}
-
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+class LinkedList {
+  constructor(head) {
+    this.head = null;
+  }
+  insertFirst(data) {
+    const node = new Node(data, this.head);
+    this.head = node;
+  }
+  size() {
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count++;
+      node = node.next;
+    }
+    return count;
+  }
+  getFirst() {
+    let node = this.head;
+    return node;
+  }
+  getLast() {
+    let node = this.head;
+    if (!node) {
+      return null;
+    }
+    while (node) {
+      if (!node.next) {
+        return node;
+      }
+      node = node.next;
+    }
+  }
+  clear() {
+    this.head = null;
+  }
+  removeFirst() {
+    if (!this.head) {
+      return;
+    }
+    this.head = this.head.next;
+  }
+  removeLast() {
+    if (!this.head) {
+      return;
+    }
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+    let previous = this.head;
+    let node = this.head.next;
+    while (node.next) {
+      previous = node;
+      node = node.next;
+    }
+    previous.next = null;
+  }
+  insertLast(data) {
+    let last = this.getLast();
+    if (last) {
+      last.next = new Node(data);
+    } else {
+      this.head = new Node(data, this.head);
+    }
+  }
+  getAt(index) {
+    if (!this.head) {
+      return null;
+    }
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+      counter++;
+      node = node.next;
+    }
+    return null;
+  }
+  removeAt(index) {
+    if (!this.head) {
+      return;
+    }
+    if (index === 0) {
+      this.head = this.head.next;
+      return;
+    }
+    const previous = this.getAt(index - 1);
+    if (!previous || !previous.next) return;
+    previous.next = previous.next.next;
+  }
+}
 module.exports = { Node, LinkedList };
